6095af2807b314d18e22d8f79e7ca4415b705400
victor <victsantotm@hotmail.com>
2024-09-18 11:58:35 +0200

    Generate api lib

---
+++ libs/api/src/lib/api.tsx
@@ -0,0 +1,11 @@
+import styles from './api.module.css';
+
+export function Api() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Api!</h1>
+    </div>
+  );
+}
+
+export default Api;
